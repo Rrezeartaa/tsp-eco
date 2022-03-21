77aadb143d6f6c16f68ac56b2229576deeeec338
Rrezeartaa <rrezearta.thaqi@student.uni-pr.edu>
2022-03-21 19:50:24 +0100

    Initial commit

---
+++ ga.py
@@ -0,0 +1,222 @@
+import sys
+import argparse
+import time
+import random
+import numpy as np
+from ls_2opt import loadTSP
+from ls_2opt import localSearch
+from ls_2opt import localOptimize
+
+def initPopulation(popsize, n, dm, maxiter):
+	
+	start_l = []
+
+	solutions = []
+	fitness = []
+
+	i = 0
+	for i in range(1,popsize+1):
+		np.random.seed(i)
+
+		s,cost,init_cost,t = localSearch(n, dm,
+						init_strategy='greedy',
+						n_strategy='best_improve',
+						maxiter=maxiter,
+						start_l=start_l)
+		solutions.append(s)
+		fitness.append(cost)
+		if t == maxiter:
+			print("Warning: maxiter reached in initializations local search.")
+	# Kthimi i popullates si nje tuple i fitnesit dhe zgjidhjeve
+	return (np.array(solutions), np.array(fitness))
+
+def selectParents(population, strategy='tournament', k=2):
+	
+	if strategy == 'tournament':
+		I = [i for i in range(0, population[1].size)]
+		parents = []
+		for parent in [1, 2]:
+			pool = random.sample(I,k)
+			
+			rel_winner_i = np.argmin(population[1][pool]) # O(k)
+			winner_i = pool[rel_winner_i]
+			parents.append(population[0][winner_i])
+			# Largimi i fituesit qe mos me zgjedh perseri
+			I.remove(winner_i)
+		return parents[0], parents[1]
+
+	sys.exit("Selection Mechanism for Parents not supported.")
+
+def addEdge(child, parent, dm):
+	
+	cities = dm.shape[0]
+	len_o = len(child)
+	l_c = child[len_o - 1]
+
+	r_i = parent.index(l_c)
+
+	if r_i > 0:
+		p_c = parent[r_i - 1]
+	else:
+		p_c = parent[cities - 1]
+	if r_i < cities - 1:
+		n_c = parent[r_i + 1]
+	else:
+		n_c = parent[0]
+
+	p_flag = True
+	n_flag = True
+	if p_c in child:
+		p_flag = False
+	if n_c in child:
+		n_flag = False
+	if p_flag and n_flag:
+		if dm[l_c,p_c] < dm[l_c,n_c]:
+			child.append(p_c)
+		else: 
+			child.append(n_c)
+	elif p_flag:
+		child.append(p_c)
+	elif n_flag:
+		child.append(n_c)
+	else:
+		return False
+	return True
+
+def crossover(p1, p2, strategy='MPX', dm=None):
+	
+	if strategy == 'OX':
+		p1 = p1.tolist()
+		p2 = p2.tolist()
+
+		last_city = p1.pop()
+
+		del p2[len(p2)-1]
+		k = int(1/3 * len(p1)) 
+		offspring = []
+
+		while p1:
+			p11_len = min(len(p1),k)
+			p11 = p1[:p11_len]
+			offspring.extend(p11)
+			p1 = p1[p11_len:]
+
+			p2 = [city for city in p2 if city not in p11]
+
+			tmp = p1
+			p1 = p2
+			p2 = tmp
+		offspring.append(last_city)
+		return offspring
+
+	if strategy == 'MPX':
+		donor = p1.tolist()
+		receiver = p2.tolist()
+
+		cities = len(donor) - 1
+		del donor[cities]
+		del receiver[cities]
+		i = np.random.randint(0,cities)
+		k = np.random.randint(int(0.2 * cities), int(0.4 * cities))
+
+		offspring = []
+		j = (i + k) % cities
+		if j > i:
+			offspring = donor[i:j+1]
+		else:
+			offspring = donor[i:] + donor[:j+1]
+
+		while len(offspring) < cities:
+
+			if not addEdge(offspring, receiver, dm):
+
+				if not addEdge(offspring, donor, dm):
+
+					len_o = len(offspring)
+					l_c = offspring[len_o - 1]
+					r_i = receiver.index(l_c)
+					c_i = (r_i + 2) % cities
+					while receiver[c_i] in offspring:
+						c_i += 1
+						c_i = c_i % cities
+					offspring.append(receiver[c_i])
+		offspring.append(offspring[0])
+		return offspring
+
+	sys.exit("Choosen Crossover Strategy not supported.")
+
+def main(argv):
+	parser = argparse.ArgumentParser(description='Solve TSP.')
+	parser.add_argument('path', help='Path to .tsp file.')
+	parser.add_argument('-ps', '--popsize', help='Size of the population.', type=int, default=10)
+	parser.add_argument('-mg', '--maxgen', help='Maximum number of generations.', type=int, default=10000)
+	parser.add_argument('-mi', '--maxiter', help='Maximum number of iterations in each local search.', type=int, default=1000)
+	parser.add_argument('-sel', '--selection', help='Define selection strategy for parents.', choices=['tournament'], default='tournament')
+	parser.add_argument('-co', '--crossover', help='Define crossover strategy. ', choices=['OX','MPX'], default='MPX')
+	parser.add_argument('-st', '--stop', help='How many generation should produce no better offspring for the algorithm to stop.', type=int, default=-1)
+	args = parser.parse_args(argv[1:])
+	
+	if args.stop < 1:
+		args.stop = args.popsize
+
+	dm = loadTSP('instance.tsp')
+	n = dm.shape[0]
+
+	start_t = time.process_time()
+
+	population = initPopulation(args.popsize, n, dm, args.maxiter)
+	end_t = time.process_time()
+	diff_t_init = end_t - start_t
+	print("Successfully generated ", args.popsize, " individuals in ", diff_t_init,"s.")
+	print("Avg. time per individual: ", diff_t_init / args.popsize)
+
+	start_t = time.process_time()
+	best_fitness = min(population[1])
+	best_gen = 0
+	counter = 0
+	worst_fitness = max(population[1])
+	worst_index = np.argmax(population[1]) 
+	print("Worst fitness: ", worst_fitness)
+
+	for gen in range(0,args.maxgen):
+
+		if counter >= args.stop:
+			print("Genetic Algorithm ", gen, ".",
+				  "Could not generate a better offspring for ", counter, 
+				  " generations.")
+			break
+		counter += 1
+		random.seed(gen)
+
+		p1, p2 = selectParents(population, args.selection)
+
+		child = crossover(p1, p2, args.crossover, dm)
+
+		child, cost, init_cost, t = localOptimize(child,dm,'best_improve',args.maxiter)
+
+		if cost < worst_fitness:
+
+			if not np.isin([cost], population[1])[0]:
+				population[0][worst_index] = child
+				population[1][worst_index] = cost
+
+				worst_fitness = max(population[1])
+				worst_index = np.argmax(population[1]) 
+
+				if cost < best_fitness:
+					best_fitness = cost
+					best_gen = gen
+				print("Generation ", gen, " produced a fitter child with cost: ", cost)
+				counter = 0
+	best_fitness = min(population[1])
+	end_t = time.process_time()
+	diff_t = end_t - start_t
+	print("Successfully evolved", gen," generations in ", diff_t,"s.")
+	print("Avg. time per generation: ", diff_t / gen)
+	print("Best solution cost: ", best_fitness, " evolved in generation: ", best_gen)
+	print("Avg. solution quality: ", sum(population[1]) / args.popsize)
+	print("Generated ", args.popsize, " individuals in ", diff_t_init,"s.")
+	print("Avg. time per individual for initialization: ", diff_t_init / args.popsize)
+
+if __name__ == '__main__':
+	main(sys.argv)
---
+++ ls_2opt.py
@@ -0,0 +1,250 @@
+import sys
+import argparse
+import time
+import numpy as np
+from scipy import spatial
+
+def loadTSP(path):
+
+	city_coord = []
+	EUC_2D = False
+	EXPLICIT = False
+	n = 0
+	distance_matrix = 0
+	row = 0
+	col = 0
+	# parse file
+	with open(path) as f:
+		data = False
+		for line in f:
+			if data:
+				if 'EOF' in line or 'DISPLAY_DATA_SECTION' in line:
+					data = False
+				elif EUC_2D:
+					# Read Coordinate
+					city_coord.append(line.split(' ')[1:3])
+				elif EXPLICIT:
+					# Parse Distance Matrix
+					for el in line.split(' '):
+						if el == '':
+							continue
+						if row == col:
+							row = row + 1
+							col = 0
+							continue
+						distance_matrix[row,col] = el
+						distance_matrix[col,row] = el
+						col = col + 1
+			if not data:
+				if 'DIMENSION' in line:
+					dim_str = line.split(':')
+					n = int(dim_str[1])
+				if 'EDGE_WEIGHT_TYPE' in line:
+					if 'EUC_2D' in line:
+						EUC_2D = True
+					elif 'EXPLICIT' in line:
+						EXPLICIT = True
+					else:
+						sys.exit("File format not supported.")
+				if EXPLICIT and 'EDGE_WEIGHT_FORMAT' in line:
+					if 'LOWER_DIAG_ROW' not in line:
+						sys.exit("File format not supported.")
+				if EUC_2D and 'NODE_COORD_SECTION' in line:
+					data = True
+				if EXPLICIT and 'EDGE_WEIGHT_SECTION' in line:
+					data = True
+					distance_matrix = np.zeros((n,n), dtype=np.int32)
+	if EUC_2D:
+		city_coord_np = np.array(city_coord, dtype=np.float32)
+
+		distance_matrix = spatial.distance_matrix(city_coord_np, city_coord_np, 2)
+		np.around(distance_matrix,decimals=0,out=distance_matrix)
+	return distance_matrix
+
+def evalTwoOpt(s,i,j,dm):
+
+	if i == j - 1 or i == j or i == j + 1:
+		sys.exit("i is not allowed to be j-1, j or j+1! i="+str(i)+" j="+str(j))
+
+	d_cost = dm[s[i],s[j]] + dm[s[i+1], s[j+1]]
+	d_cost = d_cost - dm[s[i],s[i+1]] - dm[s[j],s[j+1]]
+	return d_cost
+
+def twoOpt(s,i,j):
+
+	s_new = s[:]
+
+	s_new[i+1:j+1] = reversed(s_new[i+1:j+1])
+	return s_new
+
+def genNewSolution(s, cost, dm, strategy):
+	count = 0
+	n = dm.shape[0]
+	if strategy == 'best_improve':
+
+		min_d_cost = 0
+		min_ij = None
+
+		for i in range(0, n-2):
+			for j in range(i+2, n):
+				if j == n-1 and i == 0:
+					continue
+				
+				count += 1
+				d_cost = evalTwoOpt(s,i,j,dm) 
+				if d_cost < min_d_cost:
+					min_d_cost = d_cost
+					min_ij = (i,j) 
+		if min_ij is not None:
+			min_s = twoOpt(s,min_ij[0],min_ij[1]) 
+		else:
+			min_s = s[:] 
+		return min_s, cost+min_d_cost
+	if strategy == 'first_improve':
+
+		for i in range(0, n-2):
+			for j in range(i+2, n):
+				if j == n-1 and i == 0:
+					continue
+				count += 1
+				d_cost = evalTwoOpt(s,i,j,dm) #O(1)
+				if d_cost < 0:
+					return twoOpt(s,i,j), cost+d_cost 
+		return s[:], cost
+
+	sys.exit("Neighbourhood-Search-Strategy not supported.")
+
+def eval(s, dm):
+
+	cost = 0
+	n = dm.shape[0]
+	for i in range(0,n):
+		cost += dm[s[i],s[i+1]]
+	return cost
+
+def initSolution(n,strategy,dm=None,start_l=None):
+
+	if strategy == 'random':
+
+		cities = [i for i in range(0,n)]
+		s = np.random.choice(cities, size=n, replace=False).tolist()
+		s.append(s[0])
+		return s
+	if strategy == 'greedy':
+
+		cities = [i for i in range(0,n)]
+		start = np.random.randint(0,n)
+		if start_l is not None:
+			# Regenerate starting point if already used.
+			while start in start_l:
+				start = np.random.randint(0,n)
+			start_l.append(start)
+		s = [start]
+		last = start
+		while len(s) != n:
+			s_ind = np.argsort(dm[last,], kind='heapsort')
+			for i in s_ind:
+				if i not in s:
+					s.append(i)
+					last = i
+					break
+		s.append(start)
+		return s
+
+	sys.exit("Initial solution generation strategy not supported.")
+
+def localOptimize(init_solution,dm,n_strategy,maxiter):
+
+	s = init_solution
+
+	init_cost = eval(s, dm)
+	cost = init_cost
+
+	t = 0
+	for t in range(1, int(maxiter + 1)):
+
+		s_new, cost_new = genNewSolution(s, cost, dm, n_strategy)
+		if cost_new == cost:
+			break
+		s = s_new
+		cost = cost_new
+	return s, cost, init_cost, t
+
+def localSearch(n,dm,init_strategy,n_strategy,maxiter,start_l=None):
+        
+	s = initSolution(n,init_strategy,dm,start_l)
+	
+	return localOptimize(s,dm,n_strategy,maxiter)
+
+def main(argv):
+	parser = argparse.ArgumentParser(description='Solve TSP with 2-opt.')
+	parser.add_argument('path', help='Path to .tsp file.')
+	parser.add_argument('-mi', '--maxiter', help='Maximum number of Iterations.', type=int, default=1000)
+	parser.add_argument('-s', '--strategy', help='Define neighbourhood search strategy.', choices=['first_improve','best_improve'], default='best_improve')
+	parser.add_argument('-in', '--n_init_solutions', help='Number of initial solutions to test.', type=int, default=1)
+	parser.add_argument('-is', '--init_strategy', help='Define initial solution strategy.', choices=['random','greedy'], default='random')
+	args = parser.parse_args(argv[1:])
+
+	dm = loadTSP(args.path)
+	n = dm.shape[0]
+
+	if args.init_strategy == 'greedy':
+		if args.n_init_solutions > n:
+			sys.exit("Using nearest neighbour heuristic, there cannot \
+					 be more initial solutions then cities!")
+
+	sum_time = 0
+	sum_t = 0
+	sum_cost = 0
+	best_s = 0
+	best_cost = 0
+	best_time = 0
+	best_seed = 0
+	best_t = 0
+	run = 0
+	start_l = []
+	for run in range(1,args.n_init_solutions+1):
+		print("Run: ", run)
+		np.random.seed(run)
+		start_t = time.process_time()
+
+		s,cost,init_cost,t = localSearch(n, dm,
+						args.init_strategy,
+						args.strategy,
+						args.maxiter,
+						start_l)
+		
+		end_t = time.process_time()
+		diff_t = end_t - start_t
+
+		if run == 1:
+			best_s = s
+			best_cost = cost
+			best_time = diff_t
+			best_seed = run
+			best_t = t
+		elif cost < best_cost:
+			best_s = s
+			best_cost = cost
+			best_time = diff_t
+			best_seed = run
+			best_t = t
+		sum_time += diff_t
+		sum_t += t
+		sum_cost += cost
+
+		print("Solution with costs ",cost," found in ", diff_t, "s after ", t, " iterations.")
+		print("(Initial solution costs: ", init_cost, ")")
+
+	print("Statistics of multistart local search:")
+	print(best_cost, " ... costs of best solution")
+	print(best_time, " ... time of best solution")
+	print(best_t, " ... iterations till convergence for best solution")
+	print(best_seed, " ... seed of best solution")
+	print(run, " ... inital solutions generated")
+	print(sum_cost/run, " ... average quality of solution")
+	print(sum_time/run, " ... average time for solving one tsp instance")
+	print(sum_t/run, " ... average iterations till convergence")
+
+if __name__ == "__main__":
+	main(sys.argv)
